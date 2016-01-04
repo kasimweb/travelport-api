651ae5ad89aebab6f2f4bf1ef7ffe9fbfebee59d
jsudmalis <j.sudmalis@dyninno.com>
2016-01-04 16:08:56 +0200

    Moving user configs to a git-ignored file to avoid accidental commits of sensitive data.

---
+++ config-example.php
@@ -0,0 +1,7 @@
+<?php
+// set these to your own credentials and rename to config.php
+return [
+	'username' => 'my-username', // API username
+	'password' => 'my-password', // API password
+	'profile'  => 'my-profile', // Host Access Profile
+];
